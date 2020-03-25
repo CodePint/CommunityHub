377f953ec22212cbe94cdd9a8aebd9f4a30f11df
Code Pint <geddie.code@gmail.com>
2020-03-25 11:56:48 +0000

    Further work building application skeleton
    - setup postgres configuration
    - upadted settings.py
    - added models for blog posts
    - implemented basic admin panel functionality

--- pandemic_isles/settings.py
+++ pandemic_isles/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'pandemic_isles.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pandemic_isles',
+        'USER': 'piadmin',
+        'PASSWORD': 'HxHH4GEmqwmikKnaTdG2',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -123,4 +126,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'root')
 
 STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static')
+]
