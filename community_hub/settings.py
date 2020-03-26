1b88cca8bc4fdc2c30bcbaa2a3427a8ef1876173
Code Pint <geddie.code@gmail.com>
2020-03-26 12:51:50 +0000

    adding comments to settings.py and manage.py

--- community_hub/settings.py
+++ community_hub/settings.py
@@ -125,10 +125,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# App static files
 STATIC_URL = '/static/'
 
+# Production static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'root')
 
+# Project static files
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
